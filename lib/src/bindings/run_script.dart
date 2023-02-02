a88a76bcff0cbbcef2b7f73210fb987170b1990c
Frenco <hey@frenco.dev>
2023-02-02 22:36:24 +0630

    feat: add support for M1 macs

--- lib/src/bindings/run_script.dart
+++ lib/src/bindings/run_script.dart
@@ -11,10 +11,10 @@ const blobsPath = 'src/blobs/';
 /// Supported operating systems with architectures
 /// mapped to blob file extensions.
 const supported = <ffi.Abi, String>{
-  ffi.Abi.macosX64: 'macos_x64.dylib',
   ffi.Abi.windowsX64: 'windows_x64.dll',
   ffi.Abi.linuxX64: 'linux_x64.so',
-  // todo: add support for macos arm64
+  ffi.Abi.macosX64: 'macos_x64.dylib',
+  ffi.Abi.macosArm64: 'macos_arm64.dylib',
 };
 
 /// Gets the file name of blob files based on platform
